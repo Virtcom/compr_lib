ed4a056fc3b2ebeee2defc2be71243a0a7a2588a
bonbon <mail@jan-schmitt.eu>
2024-04-13 22:17:08 +0200

    added normal read and write

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,37 @@
 use std::fs::File;
-use std::io::Read;
-use std::io::Write;
+use std::io::{Read, Write, Result};
 
-pub fn read_data_bin(file_path: &str) -> String {
-    let mut file = File::open(file_path).expect("Failed to open file");
+pub fn read_data_bin(file_path: &str) -> Result<String> {
+    let mut file = File::open(file_path)?;
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).expect("Failed to read file");
-    let mut binary_string = String::new();
-    for byte in buffer {
-        binary_string.push_str(&format!("{:08b}", byte));
-    }
-    binary_string
+    file.read_to_end(&mut buffer)?;
+    let binary_string: String = buffer.iter().map(|byte| format!("{:08b}", byte)).collect();
+    Ok(binary_string)
 }
 
-pub fn write_data_bin(binding: String, outfilename: &str) {
+pub fn write_data_bin(binding: String, outfilename: &str) -> Result<()> {
     let binary_string = binding.as_str();
     let chunks: Vec<&str> = binary_string.as_bytes().chunks(8).map(|chunk| std::str::from_utf8(chunk).unwrap()).collect();
-    let mut file = File::create(outfilename).expect("Failed to create file");
+    let mut file = File::create(outfilename)?;
     for chunk in chunks {
-        let byte = u8::from_str_radix(chunk, 2).expect("Failed to convert binary string to byte");
-        file.write_all(&[byte]).expect("Failed to write to file");
+        let byte = u8::from_str_radix(chunk, 2).map_err(|_| std::io::Error::new(std::io::ErrorKind::InvalidData, "Failed to convert binary string to byte"))?;
+        file.write_all(&[byte])?;
     }
+    Ok(())
+}
+
+pub fn write_data(input: String, outfilename: &str) -> Result<()> {
+    let mut file = File::create(outfilename)?;
+    file.write_all(input.as_bytes())?;
+    Ok(())
+}
+
+pub fn read_data(file_path: &str) -> Result<String> {
+    let mut f = File::open(file_path)?;
+    let mut buffer = Vec::new();
+    f.read_to_end(&mut buffer)?;
+    let contents = String::from_utf8(buffer).expect("Found invalid UTF-8");
+    Ok(contents)
 }
 
 #[cfg(test)]
@@ -29,7 +40,9 @@ mod tests {
     #[test]
     fn it_works() {
         let file_path = "Cargo.toml";
-        let binding = read_data_bin(file_path);
-        write_data_bin(binding, "Output");
+        let binding = read_data_bin(file_path).expect("Failed to read binary data");
+        write_data(binding, "temp").expect("Failed to write binary data");
+        let binding = read_data("temp").expect("Failed to read binary data from temp file");
+        write_data_bin(binding, "Output").expect("Failed to write binary data to Output");
     }
 }
